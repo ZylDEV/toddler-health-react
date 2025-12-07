b8147137f086a0ca83826f95d577470ecd4684f5
ZylDEV <mfauzyl36@gmail.com>
2025-12-07 18:24:19 +0900

    Move Firebase config to environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,16 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2pvrkMaMFG-OgaVrvYT3vGlXJt8hMmPE",
-  authDomain: "posyandu-bouhgenvil.firebaseapp.com",
-  databaseURL: "https://posyandu-bouhgenvil-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "posyandu-bouhgenvil",
-  storageBucket: "posyandu-bouhgenvil.firebasestorage.app",
-  messagingSenderId: "955345269022",
-  appId: "1:955345269022:web:bee534413eeee3f720590f",
-  measurementId: "G-XCRBCFE0NE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app); // <-- penting
+export const db = getDatabase(app);
